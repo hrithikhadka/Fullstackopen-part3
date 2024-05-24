d2f4970318b74641834be1c8433f5d9acbc67639
Hrithik Khadka <hritik.khadka@centria.fi>
2024-05-24 23:00:45 +0300

    3.8*: Phonebook backend step 8

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+
+app.use(
+  // morgan(":method :url")
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
