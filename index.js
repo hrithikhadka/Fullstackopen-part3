5a2ad323e9c74bafe841d85803225e96bedebedc
Hrithik Khadka <hritik.khadka@centria.fi>
2024-06-14 14:53:20 +0300

    add dist folder to backend

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("dist"));
+
 morgan.token("body", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
 });
