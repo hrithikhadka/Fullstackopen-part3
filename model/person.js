e261240c63d270e8b30db66bea6471ee4a696145
hrithikhadka <hrithikhadka00@gmail.com>
2024-11-11 21:41:26 +0200

    3.20*: Phonebook database, step 8

--- model/person.js
+++ model/person.js
@@ -23,7 +23,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    minLength: 8,
+  },
 });
 
 personSchema.set("toJSON", {
